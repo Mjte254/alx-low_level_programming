98b4ca2b6bc4570c60bef8f08bfee4bf9c6a98a8
Mjte254 <ombogimaxi96@gmail.com>
2023-03-17 00:27:07 +0300

    4. Hello, puts
    make necessary changes to add Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -13,4 +14,16 @@ int main(void)
 
 			return (0);
 
+=======
+#include<stdio.h>
+/**
+   * main - Entry point
+    *
+     * Return: Always 0 (Success)
+      */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+>>>>>>> 30cedef (4. Hello, puts)
 }
