ba4a40e0ce6f85088f94a3f181ad59657e361d3b
Mjte254 <ombogimaxi96@gmail.com>
2023-03-17 00:30:39 +0300

    5. Hello, printf
    0x00-hello_world/5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 #include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
  */
+=======
+#include<stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+>>>>>>> ede75a3 (5. Hello, printf)
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
