de94b52de3ea7efa38db4a77013bcc69ae1b0600
Mjte254 <ombogimaxi96@gmail.com>
2023-03-17 11:26:11 -0700

    12. Software is eating the World
    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+<<<<<<< HEAD
  * main - entry point
  *
  * Description: print all possible combinations of two - digit numbers
@@ -31,4 +32,35 @@ int main(void)
 	}
 	putchar('\n');
 	return ('0');
+=======
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+>>>>>>> 527679b (12. Software is eating the World)
 }
