bb1fe12c2b7873a80236cd3de45a0828fdd4e363
Mjte254 <ombogimaxi96@gmail.com>
2023-03-17 11:14:57 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success
    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+<<<<<<< HEAD
  * main - prints all combination of single digits
  * Return: Always 0
  */
@@ -12,6 +13,19 @@ int main(void)
 	{
 		putchar((i % 10) + '0');
 		if (i != 19)
+=======
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+>>>>>>> 1c11580 (9. Patience, persistence and perspiration make an unbeatable combination for success)
 		{
 			putchar(',');
 			putchar(' ');
