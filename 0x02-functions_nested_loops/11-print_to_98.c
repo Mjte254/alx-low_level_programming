c0e579c31121c46837bc0c1b7553275bce633cfe
Mjte254 <ombogimaxi96@gmail.com>
2023-03-21 14:32:52 -0700

    11. 98 Battery Street, the OG
    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,7 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * print_to_98 - print all numbers from input to 98
  * @n: the starting number
  */
@@ -19,4 +20,35 @@ void print_to_98(int n)
 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
+=======
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
+	void print_to_98(int n)
+	{
+		int i, j;
+
+		if (n <= 98)
+		{
+			for (i = n; i <= 98; i++)
+			{
+				if (i != 98)
+					printf("%d, ", i);
+				else if (i == 98)
+					printf("%d\n", i);
+
+		}
+		}
+		else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--)
+			{
+				if (j != 98)
+					printf("%d, ", j);
+				else if (j == 98)
+					printf("%d\n", j);
+			}
+		}
+>>>>>>> 586d15e (11. 98 Battery Street, the OG)
 }
