26e27bbd15c13208a47a14a371e95269a56a7716
Mjte254 <ombogimaxi96@gmail.com>
2023-03-21 14:17:22 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life
    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
@@ -21,4 +22,30 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
+=======
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+		int i, j;
+
+		i = 0;
+
+		while (i < 24)
+		{
+			j = 0;
+			while (j < 60)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+				_putchar('\n');
+				j++;
+			}
+			i++;
+		}
+>>>>>>> 70c09eb (8. I'm federal agent Jack Bauer, and today is the longest day of my life)
 }
