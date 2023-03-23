8a2e64f905f096ce4be1bfcdcb66d3b0dfab9931
Mjte254 <ombogimaxi96@gmail.com>
2023-03-23 16:48:07 -0700

    1. Like, comment, subscribe
    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,6 +7,7 @@
 
 int main(void)
 {
+<<<<<<< HEAD
 	int i;
 
 	printf("Infinite loop incoming :(\n");
@@ -21,4 +22,20 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
+=======
+ int i;
+
+ printf("Infinite loop incoming :(\n");
+
+ i = 0;
+
+ /*while (i < 10)*/
+ /*{*/
+ /* putchar(i);*/
+ /*}*/
+
+ printf("Infinite loop avoided! \\o/\n");
+
+ return (0);
+>>>>>>> bb63ac7 (1. Like, comment, subscribe)
 }
