13d0124ae6b89a8c392009ad169df76709eb2b38
Mjte254 <ombogimaxi96@gmail.com>
2023-03-24 14:53:43 -0700

    0. isupper
    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,20 @@
 
 /**
  * _isupper - uppercase letters
+<<<<<<< HEAD
  * @c: character to check
+=======
+ * @c: char to check
+ *
+>>>>>>> 2c5c273 (0. isupper)
  * Return: 0 or 1
  */
 
 int _isupper(int c)
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2c5c273 (0. isupper)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 more functions nested loops
+=======
+0x04. C - More functions, more nested loops
+>>>>>>> 2c5c273 (0. isupper)
