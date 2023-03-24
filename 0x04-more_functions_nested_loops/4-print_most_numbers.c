5fd102eb69baf0b9bf8783d8eaf724f272d6f434
Mjte254 <ombogimaxi96@gmail.com>
2023-03-24 15:38:05 -0700

    4. I believe in numbers and signs
    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * print_most_numbers - prints
  * Return: void
+=======
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
+>>>>>>> 2cb051f (4. I believe in numbers and signs)
  */
 
 void print_most_numbers(void)
