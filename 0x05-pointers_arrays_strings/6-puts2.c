f7625c55e3b3330051d6d426213dbb8bb6a376c6
Mjte254 <ombogimaxi96@gmail.com>
2023-03-27 12:24:54 -0700

    6. Half the lies they tell about me aren't true
    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+<<<<<<< HEAD
 
 /**
  * puts2 - prints every other character of a string
@@ -13,5 +14,33 @@ void puts2(char *str)
 	for (string = 0; str[string] != '\0'; string++)
 		if (string % 2 == 0)
 			_putchar(str[string]);
+=======
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+>>>>>>> b68b69a (6. Half the lies they tell about me aren't true)
 	_putchar('\n');
 }
