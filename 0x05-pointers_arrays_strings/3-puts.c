637649efa6f1f2f46b1e7f570effdbceb59b42c7
Mjte254 <ombogimaxi96@gmail.com>
2023-03-27 11:50:06 -0700

    3. I do not fear computers. I fear the lack of them
    _putchar.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,26 @@
 #include "main.h"
+<<<<<<< HEAD
 
 /**
  * _puts - prints a string
  * @str: string to be printed
  */
 
+=======
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+>>>>>>> db82ed2 (3. I do not fear computers. I fear the lack of them)
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
+<<<<<<< HEAD
 	_putchar('\n');
+=======
+		_putchar('\n');
+>>>>>>> db82ed2 (3. I do not fear computers. I fear the lack of them)
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
 /**
+<<<<<<< HEAD
  * _putchar - write character c to stdout
  * @c: char to be printed
  * Return: On success 1
@@ -9,4 +10,15 @@
 int _putchar(char c)
 {
 	return (write (1, &c, 1));
+=======
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+>>>>>>> db82ed2 (3. I do not fear computers. I fear the lack of them)
 }
