83674015a8ee8e0e858ec06b156dda89946a42ed
Mjte254 <ombogimaxi96@gmail.com>
2023-03-29 14:01:12 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work
    0x06-pointers_arrays_strings/102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,27 +16,37 @@ p[5] 98;
 >>>>>>> 2c0d677 (10. A dream doesn't become reality through magic; it takes sweat, determination and hard work)
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+int n;
+int a[5];
+int *p;
 
+<<<<<<< HEAD
 	a[2] = 1024;
 	p = &n;
 <<<<<<< HEAD
 	printf("a[2] = %d\n", a[2]);
 
 =======
+=======
+a[2] = 1024;
+p = &n;
+>>>>>>> de521a3 (10. A dream doesn't become reality through magic; it takes sweat, determination and hard work)
 /*
-  * write your line of code here...
-  * Remember:
-  * - you are not allowed to use a
-  * - you are not allowed to modify p
-  * - only one statement
-  * - you are not allowed to code anything else than this line of code
-  */
-	*(p + 5) = 98;
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+*(p + 5) = 98;
  /* ...so that this prints 98\n */
+<<<<<<< HEAD
 	printf("a[2] = %d\n", a[2]);
 >>>>>>> 2c0d677 (10. A dream doesn't become reality through magic; it takes sweat, determination and hard work)
 	return (0);
+=======
+printf("a[2] = %d\n", a[2]);
+return (0);
+>>>>>>> de521a3 (10. A dream doesn't become reality through magic; it takes sweat, determination and hard work)
 }
