1f54214ec91ba5f7e7fe50293c5ae7d2bae25530
Mjte254 <ombogimaxi96@gmail.com>
2023-03-29 14:19:40 -0700

    12. Noise is a buffer, more effective than cubicles or booth walls
    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,14 +2,22 @@
 #include <stdio.h>
 
 /**
+<<<<<<< HEAD
  * print_buffer - prints a buffer
  * @b: buffer
  * @size: size of buffer
  * Return: no return
+=======
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+>>>>>>> 2ea9902 (12. Noise is a buffer, more effective than cubicles or booth walls)
  */
 
 void print_buffer(char *b, int size)
 {
+<<<<<<< HEAD
 	int j, k, l;
 
 	if (size <= 0)
@@ -40,5 +48,43 @@ void print_buffer(char *b, int size)
 			}
 			printf("\n");
 		}
+=======
+	int o, j, i;
+
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+>>>>>>> 2ea9902 (12. Noise is a buffer, more effective than cubicles or booth walls)
 	}
 }
