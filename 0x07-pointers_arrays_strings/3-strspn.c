b574bf58665a3a0f73df0d0f2a470d6513e8b3b3
Mjte254 <ombogimaxi96@gmail.com>
2023-04-03 07:46:29 -0700

    3. strspn
    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.c"
 
 /**
@@ -26,4 +27,33 @@ unsigned int _strspn(char *s, char *accept)
 	}
 
 	return (z);
+=======
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+>>>>>>> d446078 (3. strspn)
 }
