f6475f612d7802437943b62259367c503a9fea51
Mjte254 <ombogimaxi96@gmail.com>
2023-04-03 08:08:08 -0700

    6. Chess is mental torture
    Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.c"
 
 /**
@@ -9,13 +10,29 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+=======
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+>>>>>>> e0a382b (6. Chess is mental torture)
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
+<<<<<<< HEAD
 		{
 			_putchar(a[i][j]);
 		}
+=======
+			_putchar(a[i][j]);
+>>>>>>> e0a382b (6. Chess is mental torture)
 		_putchar('\n');
 	}
 }
