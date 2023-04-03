c85a33dfe8083681f2826af458e51afd68f6e3db
Mjte254 <ombogimaxi96@gmail.com>
2023-04-03 07:49:23 -0700

    4. strpbrk
    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.c"
 
 /**
@@ -22,4 +23,28 @@ char *_strpbrk(char *s, char *accept)
 	}
 
 	return (NULL);
+=======
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int k;
+
+		while (*s)
+		{
+			for (k = 0; accept[k]; k++)
+			{
+			if (*s == accept[k])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+>>>>>>> e5e5486 (4. strpbrk)
 }
