6622b8ea16ad78b8efde84af26281a254bb84cb0
Mjte254 <ombogimaxi96@gmail.com>
2023-04-03 07:42:28 -0700

    2. strchr
    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.c"
 
 /**
@@ -21,4 +22,23 @@ char *_strchr(char *s, char c)
 
 	return (NULL);
 
+=======
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+>>>>>>> 702a32f (2. strchr)
 }
