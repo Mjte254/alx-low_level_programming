de47f765fd21f12bce3b3a741cbd00bd674ca7d6
Mjte254 <ombogimaxi96@gmail.com>
2023-04-05 06:18:34 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything
    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -7,10 +8,36 @@
  * Return: the length of a string.
  */
 
+=======
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+>>>>>>> 612b069 (7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
+<<<<<<< HEAD
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
@@ -45,4 +72,24 @@ int is_palindrome(char *s)
 	if (*s == '\0')
 		return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
+=======
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+>>>>>>> 612b069 (7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything)
 }
