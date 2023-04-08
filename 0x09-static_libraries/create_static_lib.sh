ca4fed983e39c4e7706c7f53b49db40606cf8a0a
Mjte254 <ombogimaxi96@gmail.com>
2023-04-08 12:49:23 -0700

    1. Without libraries what have we? We have no past and no future
    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+<<<<<<< HEAD
 gcc -c *.c
 ar rc liball.a *.o
+=======
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+>>>>>>> 2e90ff7 (1. Without libraries what have we? We have no past and no future)
 ranlib liball.a
