b002cf9e9c327de13937ae1011e38a97183b1447
Mjte254 <ombogimaxi96@gmail.com>
2023-04-11 12:55:39 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe
    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+<<<<<<< HEAD
 
 /**
  * len - returns lenth of str
@@ -30,10 +31,24 @@ char *argstostr(int ac, char **av)
 {
 	char *new_string = NULL;
 	int k = 0, i = ac, j, sum = 0, temp = 0;
+=======
+#include <stdlib.h>
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, n, r = 0, l = 0;
+	char *str;
+>>>>>>> 856a411 (5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
+<<<<<<< HEAD
 	while (ac--)
 		sum += (len(av[ac]) + 1);
 	new_string = (char *) malloc(sum + 1);
@@ -55,4 +70,29 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 	return (new_string);
+=======
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
+>>>>>>> 856a411 (5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe)
 }
