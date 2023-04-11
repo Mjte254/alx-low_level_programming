75747d7747167f6862f10442a7f978f5989f0986
Mjte254 <ombogimaxi96@gmail.com>
2023-04-11 12:49:02 -0700

    4. It's not bragging if you can back it up
    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -7,12 +8,31 @@
  * Return:returns void
  */
 
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * Description: frees memory of grid
+ * Return: nothing
+ *
+ */
+>>>>>>> 465b117 (4. It's not bragging if you can back it up)
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
+<<<<<<< HEAD
 		free(grid[i]);
 
+=======
+	{
+		free(grid[i]);
+	}
+>>>>>>> 465b117 (4. It's not bragging if you can back it up)
 	free(grid);
 }
