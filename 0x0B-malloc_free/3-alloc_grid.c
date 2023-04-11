10d3df3a5a7f31bdabe4f89bdb5b0be66cbd403a
Mjte254 <ombogimaxi96@gmail.com>
2023-04-11 12:33:44 -0700

    3. If you even dream of beating me you'd better wake up and apologize
    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+<<<<<<< HEAD
 
 /**
  * alloc_grid - returns a pointer to a
@@ -43,4 +44,47 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
+=======
+#include <stdlib.h>
+/**
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **mee;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	mee = malloc(sizeof(int *) * height);
+
+	if (mee == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		mee[x] = malloc(sizeof(int) * width);
+
+		if (mee[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(mee[x]);
+
+			free(mee);
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+
+	return (mee);
+>>>>>>> e7b1442 (3. If you even dream of beating me you'd better wake up and apologize)
 }
