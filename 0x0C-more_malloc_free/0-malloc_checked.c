dd18df041e35272ab3ae6753d6e00bbc7e343e32
Mjte254 <ombogimaxi96@gmail.com>
2023-04-13 14:41:47 -0700

    0. Trust no one
    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "main.h"
 #include <stdlib.h>
 
@@ -15,4 +16,25 @@ void *malloc_checked(unsigned int b)
 	if (p == NULL)
 		exit(98);
 	return (p);
+=======
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+>>>>>>> 99d341f (0. Trust no one)
 }
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 	Using the existing Functions...
 
 	What are the functions calloc and realloc from the standard library?
 
 	How to use them
+=======
+0x0C. C - More malloc, free
+>>>>>>> 99d341f (0. Trust no one)
