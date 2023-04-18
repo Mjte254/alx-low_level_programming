3c51e0a627feb1296f2383dafaac489c24da3c53
Mjte254 <ombogimaxi96@gmail.com>
2023-04-18 13:22:26 -0700

    3. Function-like macro
     Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef THREE_FUNCTION_LIKE_MACRO_H
 
 #define THREE_FUNCTION_LIKE_MACRO_H
@@ -5,3 +6,11 @@
 #define ABS(x) (x > 0 ? (x) : (x) * -1)
 
 #endif /* THREE_FUNCTION_LIKE_MACRO_H */
+=======
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
+>>>>>>> 9847bdd (3. Function-like macro)
