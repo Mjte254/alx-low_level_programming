3bd3af9da5aab32fe2d1bb1e9afb097a31eec799
Mjte254 <ombogimaxi96@gmail.com>
2023-04-18 13:25:30 -0700

    4. SUM
    SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #ifndef FOUR_SUM_H
 
 #define FOUR_SUM_H
@@ -5,3 +6,11 @@
 #define SUM(x, y) ((x) + (y))
 
 #endif /* FOUR_SUM_H */
+=======
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+>>>>>>> 5417e90 (4. SUM)
