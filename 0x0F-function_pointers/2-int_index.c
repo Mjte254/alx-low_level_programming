f3674635a93cf86210c7b7acaf80ee4b353793f8
Mjte254 <ombogimaxi96@gmail.com>
2023-04-19 03:40:17 -0700

    2. To hell with circumstances; I create opportunities
    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+<<<<<<< HEAD
 
 /**
  * int_index - earches for an integer
@@ -10,10 +11,20 @@
  * or size is negative
  */
 
+=======
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+>>>>>>> 2bd3e70 (2. To hell with circumstances; I create opportunities)
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+<<<<<<< HEAD
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
@@ -23,5 +34,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 	}
 
+=======
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+>>>>>>> 2bd3e70 (2. To hell with circumstances; I create opportunities)
 	return (-1);
 }
