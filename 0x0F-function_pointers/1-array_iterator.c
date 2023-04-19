4afddc57d5523b19297cf3d7bdc6d40ae8a7e4c4
Mjte254 <ombogimaxi96@gmail.com>
2023-04-19 03:34:56 -0700

    1. If you spend too much time thinking about a thing, you'll never get it done
    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdlib.h>
 #include "function_pointers.h"
 
@@ -9,13 +10,34 @@
  * @action: pointer to function used
  */
 
+=======
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
+ */
+>>>>>>> afa67ca (1. If you spend too much time thinking about a thing, you'll never get it done)
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+<<<<<<< HEAD
 	if (!array || !action)
 		return;
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
+=======
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+>>>>>>> afa67ca (1. If you spend too much time thinking about a thing, you'll never get it done)
 }
