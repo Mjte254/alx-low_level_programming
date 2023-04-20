7f01b3ac7e71aef73f0241f0f72f0e56ba9c127c
Mjte254 <ombogimaxi96@gmail.com>
2023-04-20 02:38:19 -0700

    1. To be is to be the value of a variable
    To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+<<<<<<< HEAD
 
 /**
  * print_numbers - prints num
@@ -17,9 +18,37 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(valist, int));
 		if (separator && i < n - 1)
+=======
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+
+		if (index != (n - 1) && separator != NULL)
+>>>>>>> 822b350 (1. To be is to be the value of a variable)
 			printf("%s", separator);
 	}
 
 	printf("\n");
+<<<<<<< HEAD
 	va_end(valist);
+=======
+
+	va_end(nums);
+>>>>>>> 822b350 (1. To be is to be the value of a variable)
 }
