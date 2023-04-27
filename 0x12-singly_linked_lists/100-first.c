2ea5b9f3c654bd72ca6a1e1709f56f7cde8516d4
Mjte254 <ombogimaxi96@gmail.com>
2023-04-27 12:45:18 -0700

    5. The Hare and the Tortoise
    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "lists.h"
 
 /**
@@ -9,4 +10,18 @@ void __attribute__ ((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
+=======
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+>>>>>>> 332ac22 (5. The Hare and the Tortoise)
 }
