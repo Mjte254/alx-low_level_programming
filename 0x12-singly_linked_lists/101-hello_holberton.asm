6c0028d2830dc989e24e3d83d927414c4e41e2d8
Mjte254 <ombogimaxi96@gmail.com>
2023-04-27 12:52:21 -0700

    6. Real programmers can write assembly code in any language
    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 	SECTION .data
 msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
@@ -13,3 +14,15 @@ main:
 
 	mov eax, 0
 	ret
+=======
+	global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
+
+>>>>>>> 2c60e7e (6. Real programmers can write assembly code in any language)
