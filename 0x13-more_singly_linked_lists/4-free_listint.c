f66d287439ca911caabd54d3351727b63849cb71
Mjte254 <ombogimaxi96@gmail.com>
2023-05-02 15:06:22 -0700

    4. Free list
    Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,16 +3,27 @@
 /**
  * free_listint - frees a linked list
  * @head: listint_t list to be freed
+<<<<<<< HEAD
  *
  * Return: return nothing
+=======
+>>>>>>> dc46df8 (4. Free list)
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
+<<<<<<< HEAD
 	while ((temp = head) != NULL)
 	{
 		head = head->next;
 		free(temp);
+=======
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+>>>>>>> dc46df8 (4. Free list)
 	}
 }
