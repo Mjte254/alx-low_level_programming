52c70632d559f6e445cb78a7a10fa19b11b36168
Mjte254 <ombogimaxi96@gmail.com>
2023-05-04 15:29:27 -0700

    101
    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * flip_bits - returns the number of bits you would
  * need to flip to get from one number to another
  * @n: number one.
@@ -19,4 +20,27 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	}
 
 	return (nbits);
+=======
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+>>>>>>> b07ea0c (101)
 }
--- 0x14-bit_manipulation/5-main.c
+++ 0x14-bit_manipulation/5-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+<<<<<<< HEAD
 
 #include "main.h"
 
@@ -40,4 +41,26 @@ int main(void)
 
     return (0);
 
+=======
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = flip_bits(1024, 1);
+	printf("%u\n", n);
+	n = flip_bits(402, 98);
+	printf("%u\n", n);
+	n = flip_bits(1024, 3);
+	printf("%u\n", n);
+	n = flip_bits(1024, 1025);
+	printf("%u\n", n);
+	return (0);
+>>>>>>> b07ea0c (101)
 }
