6d264eb71b99fb258a650599b19233e1116b905e
Mjte254 <ombogimaxi96@gmail.com>
2023-05-04 14:47:18 -0700

    0
    0-binary_to_uint.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,13 +1,24 @@
+<<<<<<< HEAD
 #include <unistd.h>
 
+=======
+#include "main.h"
+#include <unistd.h>
+>>>>>>> 9aeefa4 (0)
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
+<<<<<<< HEAD
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 
+=======
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+>>>>>>> 9aeefa4 (0)
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
