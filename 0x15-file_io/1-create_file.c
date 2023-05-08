c7b0cd7f6082a24a0de0ce134a335758719d40fe
Mjte254 <ombogimaxi96@gmail.com>
2023-05-08 14:00:20 -0700

    Under the snow
    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * create_file - creates a file
  * @filename: filename.
  * @text_content: content writed in the file.
@@ -29,6 +30,32 @@ int create_file(const char *filename, char *text_content)
 	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
+=======
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+>>>>>>> f89ef5c (Under the snow)
 		return (-1);
 
 	close(fd);
