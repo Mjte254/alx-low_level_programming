4395c082697426465dc8ba64f800b33a43c4ca29
Mjte254 <ombogimaxi96@gmail.com>
2023-05-08 14:03:53 -0700

    Speak gently, she can hear
    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+<<<<<<< HEAD
  * append_text_to_file - appends text at the end of a file
  * @filename: filename.
  *  @text_content: added content.
@@ -12,10 +13,24 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int nletters;
 	int rwr;
+=======
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+>>>>>>> beea111 (Speak gently, she can hear)
 
 	if (filename == NULL)
 		return (-1);
 
+<<<<<<< HEAD
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
@@ -33,6 +48,21 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
+=======
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+>>>>>>> beea111 (Speak gently, she can hear)
 
 	return (1);
 }
